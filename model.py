bb1309c6a0c41ea77aaf1c4f6a24c8038058c4c9
Mellody <mellodynguyen@outlook.com>
2024-01-08 23:43:13 -0600

    fixed images table - rcvd approval

--- model.py
+++ model.py
@@ -16,12 +16,14 @@ class User(db.Model):
     screenname = db.Column(db.String, nullable=False)
 
     # a user can have many posts
-    posts = db.relationship("Post", back_populates="user")
+    user_posts = db.relationship("Post", back_populates="user")
 
 
     # data model lecture example:
     # book = db.relationship("Book", back_populates="printings")
-    # Model(Book) name and table name(Printings)?
+
+    # Class(Book) name and back_populates is how you relate the attributes in
+    # the difference classes to each other 
 
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email}>'
@@ -35,19 +37,29 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
     # what if the body has a picture/video too?
     body = db.Column(db.String, nullable=False)
-    hasimage = db.Column(db.Boolean, nullable=False)
+
+    # can get rid of hasimage because of the relationship (line 54)
+    # hasimage = db.Column(db.Boolean, nullable=False)
+
     # DateTime format?
     timestamp = db.Column(db.DateTime)
     language = db.Column(db.String, default="english")
 
     # one user can have many posts
-    user = db.relationship("User", back_populates="posts")
+    user = db.relationship("User", back_populates="user_posts")
 
     # a post can have many replies
     reply = db.relationship("Reply", back_populates="replies")
 
     # a post can have many likes
     like = db.relationship("PostLike", back_populates="postlikes")
+
+    # multiple images to a post (one to many) but there will only be one post
+    images = db.relationship("Images", back_populates="post")
+
+    # testing purposes:
+    # post.images will be a list and will have an image objects, 
+    # and if it doesnt, the list will be empty (no images)
 
     def __repr__(self):
         return f'<Post post_id={self.post_id}>'
@@ -58,8 +70,10 @@ class Images(db.Model):
     __tablename__ = "images"
 
     image_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.post_id"))
     image_link = db.Column(db.String, nullable=False)
+
+    post = db.relationship("Post", back_populates="images")
 
     def __repr__(self):
         return f'<Images image_id={self.image_id} image_link={self.image_link}>'
