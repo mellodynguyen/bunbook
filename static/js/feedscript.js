3b9ca30ffe7e757a390b74da0bdaf3626ceff76d
Mellody <mellodynguyen@outlook.com>
2024-01-16 18:16:04 -0600

    integrated image form with post form to allow images be added to posts

--- static/js/feedscript.js
+++ static/js/feedscript.js
@@ -1,3 +1,9 @@
-const createPostBtn = document.querySelector('#createpost')
+// const createPostBtn = document.querySelector('#createpost')
 
-createPostBtn.addEventListener('submit',)
+// createPostBtn.addEventListener('submit',)
+
+// use setInterval to send a request to the server to check if there are any 
+// new posts, every 1 minute = 60,000 milliseconds 
+
+// function checkForPosts()
+// setInterval(checkForPosts, 60000)
